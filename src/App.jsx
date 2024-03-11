7c25fd3760a887b3cc1b384554190084da1f41bb
Alexander <200300628@ucaribe.edu.mx>
2024-03-11 14:29:45 -0500

    animacion de pagina de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import './App.css'
-import { motion } from 'framer-motion'
+import React from 'react';
+import { motion } from 'framer-motion';
+import './App.css';
 
 function App() {
   return (
-   <div>
-
-      <motion.div className='box' 
-      initial={{scale:1}}
-      transition={{duration:2}}
-      animate={{scale:2, rotate:360}}
-      
-      
-      />
-
-   </div>   
-
-  )
+    <div className="container">
+      <div className="text-container">
+        <h1 className="animated-text">
+          <motion.span
+            style={{ color: '#FF0000' }}
+            whileHover={{ scale: 1.2, color: '#00FF00' }}
+            transition={{ duration: 0.2 }}
+          >
+            Harmonic
+          </motion.span>
+          <motion.span
+            style={{ color: '#FF0000' }}
+            transition={{ duration: 0.2 }}
+          >
+            Haven
+          </motion.span>
+        </h1>
+      </div>
+    </div>
+  );
 }
-export default App
+
+export default App;
 
